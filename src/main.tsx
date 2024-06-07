aa588371e4dea1d06fb1a077f046db617320702a
fabio barros <fabio.junior643@outlook.com>
2024-06-06 21:53:26 -0300

    update settings

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { routers } from './routers';
+import { CssBaseline } from '@mui/material';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <CssBaseline />
     <RouterProvider router={routers} />
   </React.StrictMode>,
 );
