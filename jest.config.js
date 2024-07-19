ab78f5a4a0e60defda5e4abcc4427a50d40e1793
fabio barros <fabio.junior643@outlook.com>
2024-07-19 12:40:53 -0300

    create alias on jest.config

--- jest.config.js
+++ jest.config.js
@@ -5,12 +5,18 @@ module.exports = {
   moduleNameMapper: {
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/.jest/fileMock.js',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '@components': '<rootDir>/src/components',
+    '@providers': '<rootDir>/src/providers',
   },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.styles.*',
+  ],
   coverageThreshold: {
     global: {
       branches: 50,
