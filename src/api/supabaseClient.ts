31990c62c8f648c2533e0f1d3fbdc4035cd88749
fabio barros <fabio.junior643@outlook.com>
2024-07-23 19:25:41 -0300

    feat: configuration to supabase tests

--- src/api/supabaseClient.ts
+++ src/api/supabaseClient.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
+const isTestMode = import.meta.env.MODE === 'test';
+const fakeUrl = 'http://fake-url.com';
+const fakeKey = '123';
+
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_KEY,
+  isTestMode ? fakeUrl : import.meta.env.VITE_SUPABASE_URL,
+  isTestMode ? fakeKey : import.meta.env.VITE_SUPABASE_KEY,
 );
