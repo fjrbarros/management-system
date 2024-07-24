9d5079dd9447c1ea3f341cfccfdfaa92b9c71ec2
fabio barros <fabio.junior643@outlook.com>
2024-07-24 18:33:30 -0300

    customRender refactor

--- src/utils/test/customRender.tsx
+++ src/utils/test/customRender.tsx
@@ -3,15 +3,13 @@ import { PropsWithChildren, ReactElement } from 'react';
 import { QueryClientProvider } from '../../providers/queryClientProvider/QueryClientProvider';
 import { ThemeProvider } from '../../providers/themeProvider/ThemeProvider';
 
+export const wrapper = ({ children }: PropsWithChildren) => (
+  <QueryClientProvider>
+    <ThemeProvider>{children}</ThemeProvider>
+  </QueryClientProvider>
+);
+
 export const customRender = (
   ui: ReactElement,
   options?: Omit<RenderOptions, 'queries'>,
-) =>
-  render(ui, {
-    wrapper: ({ children }: PropsWithChildren) => (
-      <QueryClientProvider>
-        <ThemeProvider>{children}</ThemeProvider>
-      </QueryClientProvider>
-    ),
-    ...options,
-  });
+) => render(ui, { wrapper, ...options });
