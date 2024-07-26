6f46f0c7a174fbfbfaf779365b5f96b3b4dcab40
fabio barros <fabio.junior643@outlook.com>
2024-07-26 09:20:05 -0300

    feat: create formatDate test

---
+++ src/utils/formatDate/formatDate.test.ts
@@ -0,0 +1,11 @@
+import { formatDate } from './formatDate';
+
+describe.each([
+  { date: '01-01-2021', expected: '01/01/2021' },
+  { date: 'invalid date', expected: '' },
+  { date: '10-10-2021', expected: '10/10/2021' },
+])('formatDate($date)', ({ date, expected }) => {
+  test(`returns ${expected} when date is ${date}`, () => {
+    expect(formatDate(date)).toBe(expected);
+  });
+});
--- src/utils/formatDate/formatDate.ts
+++ src/utils/formatDate/formatDate.ts
@@ -1,8 +1,15 @@
 export const formatDate = (dateString: string) => {
   const date = new Date(dateString);
+
+  if (isNaN(date.getTime())) {
+    return '';
+  }
+
   const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const dateFormatted = day < 10 ? `0${day}` : day;
   const formattedMonth = month < 10 ? `0${month}` : month;
-  const formattedDate = `${date.getDate()}/${formattedMonth}/${date.getFullYear()}`;
+  const formattedDate = `${dateFormatted}/${formattedMonth}/${date.getFullYear()}`;
 
   return formattedDate;
 };
